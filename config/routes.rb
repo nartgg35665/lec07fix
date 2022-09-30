2f8cb750f67ef0aaad22d844ff0cde2a90b67fe6
nartgg35665 <77729065+nartgg35665@users.noreply.github.com>
2022-09-30 11:13:58 +0700

    send hw

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :scores
+  resources :students
+  resources :users
+  get 'main/login'
+  get 'main/create'
+  get 'main/destroy'
+  get 'students/:id/edit_score',to:'students#edit_score'
+  # post 'studemt/:'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
